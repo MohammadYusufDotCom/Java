c050d773e4c1f32544a0b6d1dd3f2b5671acee2a
Mohammad1 Yusuf <mohammad1.yusuf@one97.delhi.net>
2024-04-09 23:57:39 +0530

    constructure

---
+++ Consctructor/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    int age;
+    String Fname;
+    
+    public Main(int x , String S){
+        age = x;
+        Fname = S;
+    } 
+} 
---
+++ Consctructor/Method.java
@@ -0,0 +1,7 @@
+public class Method {
+    public static void main(String[] args) {
+        Main objMain = new Main(25, "Mohammad");
+        objMain.Fname = "Yusuf";
+        System.out.println("First name is "+objMain.Fname+" and age is "+objMain.age);
+    }
+}
